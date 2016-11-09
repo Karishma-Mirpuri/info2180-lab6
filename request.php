bbd5f04b4911f6d0ae6c507675d0da76c55848dc
Karishma Mirpuri <karishma.mirpuri@mymona.uwi.edu>
2016-11-09 16:43:09 +0000

    Exercise 4

--- request.php
+++ request.php
@@ -1,9 +1,9 @@
 <?php
 
-// accept a term (keyword)
+/*// accept a term (keyword)
 // respond with a value
 
-$query = $_GET['q'];
+  $query = $_GET['q'];
 $definition = [
     "definition" => "A statement of the exact meaning of a word, especially in a dictionary.",
     "bar" => "A place that sells alcholic beverages",
@@ -13,6 +13,23 @@ $definition = [
     "javascript" => "A lightweight, interpreted programming language with first-class functions that adds interactivity to your website.",
     "php" => "A server-side scripting language, and a powerful tool for making dynamic and interactive websites",
 ];
-
 print "<h3>" . strtoupper($query) . "</h3>";
-print "<p>" . $definition[$query] . "</p>";
+print "<p>" . $definition[$query] . "</p>";*/
+
+  $data = '<?xml version="1.0" encoding="UTF-8"?>
+    <entries>
+        <definition name="Definition" author="-John">A statement of the exact meaning of a word, especially in a dictionary.</definition>
+        <definition name="Bar" author="-Mary">A place that sells alcholic beverages.</definition>
+        <definition name="AJAX" author="-Kimberly">Technique which involves the use of javascript and xml (or JSON)</definition>
+        <definition name="HTML" author="-Tom">The standard markup language for creating web pages and web applications.</definition>
+        <definition name="CSS" author="-Shally">A style sheet language used for describing the presentation of a document written in a markup language.</definition>
+        <definition name="Javascript" author="-Marcus">A lightweight, interpreted programming language with first-class functions that adds interactivity to your website.</definition>
+        <definition name="PHP" author="-Nick">A server-side scripting language, and a powerful tool for making dynamic and interactive websites</definition>
+    </entries>';
+    header('Content-Type: text/xml');
+    $xmlOutput = new SimpleXMLElement($data);
+    echo $xmlOutput->asXML();
+
+
+
+
